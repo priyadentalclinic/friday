c29f08006f9eac8b57d1fd7474a5c807e4fd607e
priyadentalclinic <kgaurang08@gmail.com>
2026-07-23 22:52:49 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
-      'react-native-worklets/plugin',
+      ['react-native-worklets/plugin', {}, 'react-native-worklets-plugin'],
+      ['react-native-reanimated/plugin', {}, 'react-native-reanimated-plugin'],
     ],
   };
 };
