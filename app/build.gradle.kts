8a6ec2595a4f442e392736b7b808ac2b3896f597
priyadentalclinic <gaurangkhurana13@gmail.com>
2026-07-26 20:25:26 +0530

    Align Kotlin toolchain to 2.3.0 to fix metadata mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // FRIDAY Mark VII - root build file
 plugins {
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.3.4" apply false
 }
 
 tasks.register<Delete>("clean") {
